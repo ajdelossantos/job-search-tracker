541af8fe59a441f647c571900d6547de97d1c3d4
Alvin James T. Delos Santos <a.jamesdelossantos@gmail.com>
2025-09-02 22:56:51 -0500

    fix: move db location and fix deprecated warning

--- backend/app/data/database.py
+++ backend/app/data/database.py
@@ -2,10 +2,9 @@
 
 import sqlite3
 from sqlalchemy import create_engine, event
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./jobsapp.db"
+DATABASE_URL = "sqlite:///./app/data/jobsapp.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
