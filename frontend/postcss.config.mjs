a0c5e26cb23942daffce6770ca4ff22e933de51c
Alvin James T. Delos Santos <a.jamesdelossantos@gmail.com>
2025-09-15 15:30:37 -0500

    fix: lightningcss issues

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,7 +1,8 @@
-import tailwindcss from "@tailwindcss/postcss";
-
 const config = {
-  plugins: [tailwindcss],
-};
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+}
+
 
 export default config;
