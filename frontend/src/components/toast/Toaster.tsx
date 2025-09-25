8bb27bbb0121685539f184ebf1db098650908b86
Alvin James T. Delos Santos <a.jamesdelossantos@gmail.com>
2025-09-25 13:10:35 -0500

    docs: update toaster

--- frontend/src/components/toast/Toaster.tsx
+++ frontend/src/components/toast/Toaster.tsx
@@ -3,7 +3,14 @@
 import { Toaster } from "sonner";
 
 /**
- * Toast notification component.
+ * AppToaster component that renders a toast notification system.
+ *
+ * @returns A Toaster component configured with:
+ * - Position: bottom-left of the screen
+ * - Rich colors enabled for different toast types
+ * - Close button for manual dismissal
+ * - Non-expanding behavior
+ * - 3.5 second duration for all toasts
  */
 export default function AppToaster() {
   return (
